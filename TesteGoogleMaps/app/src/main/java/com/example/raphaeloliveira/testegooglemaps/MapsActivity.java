872854c3f053b11ebf853ea3c9ff6afb7f2cf195
raphaeloliveira <oliver.raphaell@gmail.com>
2017-11-07 14:30:07 -0200

    commit com a permissão

--- TesteGoogleMaps/app/src/main/java/com/example/raphaeloliveira/testegooglemaps/MapsActivity.java
+++ TesteGoogleMaps/app/src/main/java/com/example/raphaeloliveira/testegooglemaps/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.example.raphaeloliveira.testegooglemaps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,6 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 123;
     private double LAT = 0;
     private double LONG = 0;
     private GoogleMap mMap;
@@ -24,15 +30,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-        new LocationGPS(this, new CallBack() {
-            @Override
-            public void coordenadas(Double[] resultado) {
-                LAT = resultado[0];
-                LONG = resultado[1];
-                mapFragment.getMapAsync(MapsActivity.this);
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
             }
-        });
+        }
     }
 
     @Override
@@ -47,5 +66,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
         //zoom na posição
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zoom.getCenter(), 17));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    new LocationGPS(this, new CallBack() {
+                        @Override
+                        public void coordenadas(Double[] resultado) {
+                            LAT = resultado[0];
+                            LONG = resultado[1];
+                            mapFragment.getMapAsync(MapsActivity.this);
+                        }
+                    });
+
+                } else {
+                    // permission denied
+                    // Disable the functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' statements for other permssions
+        }
     }
 }
